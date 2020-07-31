cb89b5dc384303b30b801205cb96328441074f22
Sicmatr1x <sicmatr1x@outlook.com>
2020-07-31 18:24:55 +0800

    Reformat code

--- src/main/java/com/sicmatr1x/controller/IndexController.java
+++ src/main/java/com/sicmatr1x/controller/IndexController.java
@@ -21,55 +21,58 @@ import java.util.Map;
 @RequestMapping("notebook")
 public class IndexController {
 
-  @Autowired
-  private Constant constant;
+    @Autowired
+    private Constant constant;
 
-  @Autowired
-  private SpiderService spiderService;
+    @Autowired
+    private SpiderService spiderService;
 
-  private ObjectMapper objectMapper = new ObjectMapper();
+    private ObjectMapper objectMapper = new ObjectMapper();
 
-  /**
-   * 检查版本号
-   * @return application version
-   */
-  @RequestMapping("/version")
-  public CommonVo version() {
-    Map<String, String> dataMap = new HashMap<>();
-    dataMap.put("version", constant.getVersion());
-    CommonVo response = new CommonVo(true, dataMap);
-    return response;
-  }
-
-  /**
-   * 添加知乎回答
-   * @return is add job success
-   */
-  @RequestMapping("/zhihu/question/{questionId}/answer/{answerId}")
-  public CommonVo spiderZhihuAnswer(@PathVariable String questionId, @PathVariable String answerId) {
-    CommonVo response = new CommonVo(false);
-    Article article = new Article();
-    article.setUrl(ZhihuHtmlUtil.DOMAIN + "question/" + questionId + "/answer/" + answerId);
-    article.setSource(ArticleSource.ZHIHU_ANSWER);
-    Article resultArticle = null;
-    try {
-      resultArticle = spiderService.spiderZhihuAnswer(article);
-      response.setSuccess(true);
-      // 避免返回body过大
-      resultArticle.setBody(resultArticle.getBody().substring(0, 400) + "......");
-      response.setData(resultArticle);
-    } catch (IOException e) {
-      e.printStackTrace();
-      response.setErrorMessage(e.getMessage());
+    /**
+     * 检查版本号
+     *
+     * @return application version
+     */
+    @RequestMapping("/version")
+    public CommonVo version() {
+        Map<String, String> dataMap = new HashMap<>();
+        dataMap.put("version", constant.getVersion());
+        CommonVo response = new CommonVo(true, dataMap);
+        return response;
     }
-    return response;
-  }
 
     /**
      * 添加知乎回答
+     *
      * @return is add job success
      */
-    @RequestMapping(value="/add/zhihu/answer", method = RequestMethod.POST)
+    @RequestMapping("/zhihu/question/{questionId}/answer/{answerId}")
+    public CommonVo spiderZhihuAnswer(@PathVariable String questionId, @PathVariable String answerId) {
+        CommonVo response = new CommonVo(false);
+        Article article = new Article();
+        article.setUrl(ZhihuHtmlUtil.DOMAIN + "question/" + questionId + "/answer/" + answerId);
+        article.setSource(ArticleSource.ZHIHU_ANSWER);
+        Article resultArticle = null;
+        try {
+            resultArticle = spiderService.spiderZhihuAnswer(article);
+            response.setSuccess(true);
+            // 避免返回body过大
+            resultArticle.setBody(resultArticle.getBody().substring(0, 400) + "......");
+            response.setData(resultArticle);
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.setErrorMessage(e.getMessage());
+        }
+        return response;
+    }
+
+    /**
+     * 添加知乎回答
+     *
+     * @return is add job success
+     */
+    @RequestMapping(value = "/add/zhihu/answer", method = RequestMethod.POST)
     public CommonVo spiderZhihuAnswer2(@RequestParam String url) {
         CommonVo response = new CommonVo(false);
         Article article = new Article();
@@ -92,16 +95,17 @@ public class IndexController {
 
     /**
      * 添加知乎回答
+     *
      * @return is add job success
      */
-    @RequestMapping(value="/add/zhihu/p", method = RequestMethod.POST)
+    @RequestMapping(value = "/add/zhihu/p", method = RequestMethod.POST)
     public CommonVo spiderZhihuZhuanLan(@RequestParam String url) {
         CommonVo response = new CommonVo(false);
         Article article = new Article();
         String[] work = url.split("\\?");
         article.setUrl(work[0]);
         article.setSource(ArticleSource.ZHIHU_ZHUANLAN);
-        Article resultArticle = null; 
+        Article resultArticle = null;
         try {
             resultArticle = spiderService.spiderZhihuZhuanLan(article);
             response.setSuccess(true);
@@ -115,23 +119,24 @@ public class IndexController {
         return response;
     }
 
-  /**
-   * 根据URL查找article
-   * @param url
-   * @return
-   */
-  @RequestMapping(value="/article",method = RequestMethod.GET)
-  public String findOneArticleById(@RequestParam String url) throws IOException {
-    Article article = null;
-    String[] work = url.split("\\?");
-    article = spiderService.findOneArticleByURL(work[0]);
-    if (article == null) {
-        CommonVo response = new CommonVo(true);
-        response.setErrorMessage("Not found any result in DB");
-        return objectMapper.writeValueAsString(response);
-    } else {
-        return article.getBody();
+    /**
+     * 根据URL查找article
+     *
+     * @param url
+     * @return
+     */
+    @RequestMapping(value = "/article", method = RequestMethod.GET)
+    public String findOneArticleById(@RequestParam String url) throws IOException {
+        Article article = null;
+        String[] work = url.split("\\?");
+        article = spiderService.findOneArticleByURL(work[0]);
+        if (article == null) {
+            CommonVo response = new CommonVo(true);
+            response.setErrorMessage("Not found any result in DB");
+            return objectMapper.writeValueAsString(response);
+        } else {
+            return article.getBody();
+        }
     }
-  }
 
 }
